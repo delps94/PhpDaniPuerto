75dc50d2ecad2b66c857354a7b05a6300cc678c4
delps94 <113589670+delps94@users.noreply.github.com>
2022-09-23 09:48:06 +0200

    Create ej1bucles.php

---
+++ ut1/ej1bucles.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD><TITLE> EJ1B – Conversor decimal a binario</TITLE></HEAD>
+<BODY>
+<?php
+$num="18";
+$n="0";
+while ($n<$num) {
+$n=decbin($n); 
+$n=str_pad($n,8,"0",STR_PAD_LEFT);
+$n=bindec($n);
+$n++;
+
+}
+$numbinario=decbin($n);
+$numbinariocompleto=str_pad($numbinario,8,"0",STR_PAD_LEFT);
+echo "$numbinariocompleto";
+; 	 
+?>
+</BODY>
+</HTML>
