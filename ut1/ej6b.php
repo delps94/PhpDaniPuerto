70b2e5ed89265452cf6403e94dc4822c2c5f73f4
delps94 <113589670+delps94@users.noreply.github.com>
2022-09-26 13:27:50 +0200

    Create ej6b.php

---
+++ ut1/ej6b.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD><TITLE> EJ6B – Factorial</TITLE></HEAD>
+<BODY>
+	<?php
+	$num="9";
+	echo "el factorial de $num es igual a "; 
+	$fact= "1"; 
+	for ($i=1;$i<=$num;$i++) {
+		$fact=$fact*$i;
+
+	} 
+	echo "$fact";
+
+	?>
+</BODY>
+</HTML>
