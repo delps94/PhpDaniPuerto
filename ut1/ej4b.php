2db7f75181a81ba50e88e040f95f5706d07a415c
delps94 <113589670+delps94@users.noreply.github.com>
2022-09-26 09:34:32 +0200

    Create ej4b.php

---
+++ ut1/ej4b.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+	<?php
+	$num="8";
+	for ($i=1;$i<=10;$i++) {
+		$res=$num*$i; 
+		echo ("$num x $i = $res <br>"); 
+	}
+	?>
+</BODY>
+</HTML>
